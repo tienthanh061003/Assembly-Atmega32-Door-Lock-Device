731e7c38949d7ce5c95b589c81a9e6fe7b4453c3
Mai Nguyen Tien Thanh <49037275+tienthanh061003@users.noreply.github.com>
2024-04-28 03:17:54 +0700

    Add files via upload

---
+++ AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"E:\AVR\AvrAssembler2\avrasm2.exe" -S "E:\IU\MPS_Lab_1\Project\MTNT\MTNT_Copy\labels.tmp" -fI -W+ie -C V2E -o "E:\IU\MPS_Lab_1\Project\MTNT\MTNT_Copy\MTNT.hex" -d "E:\IU\MPS_Lab_1\Project\MTNT\MTNT_Copy\MTNT.obj" -e "E:\IU\MPS_Lab_1\Project\MTNT\MTNT_Copy\MTNT.eep" -m "E:\IU\MPS_Lab_1\Project\MTNT\MTNT_Copy\MTNT.map" "E:\IU\MPS_Lab_1\Project\MTNT\MTNT_Copy\MTNT.asm"
